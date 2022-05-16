e3ebaa36459214189c4cf050e7a9794bbe00da81
Marcos Rodrigues <m4rcos.rodrig@gmail.com>
2022-05-16 20:35:37 -0300

    Adicionando screenshots ao fim do teste e mais algumas atualizações

--- src/main/java/br/ce/marcos/core/DriverFactory.java
+++ src/main/java/br/ce/marcos/core/DriverFactory.java
@@ -3,6 +3,7 @@ package br.ce.marcos.core;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 	
@@ -12,7 +13,11 @@ public class DriverFactory {
 	
 	public static WebDriver getDriver() {
 		if(driver == null) {
-			driver = new EdgeDriver();
+			switch (Propriedades.browser) {
+				case FIREFOX: driver = new FirefoxDriver(); break; 
+				case EDGE: driver = new EdgeDriver(); break; 
+				
+			}
 			driver.manage().window().setSize(new Dimension(1000, 720));
 			
 		}
--- src/main/java/br/ce/marcos/suites/SuiteTeste.java
+++ src/main/java/br/ce/marcos/suites/SuiteTeste.java
@@ -1,19 +1,23 @@
 package br.ce.marcos.suites;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import br.ce.marcos.core.DriverFactory;
 import br.ce.marcos.teste.TesteCadastro;
-import br.ce.marcos.teste.TesteCampoTreinamento;
 import br.ce.marcos.teste.TesteRegrasCadrasto;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	TesteCadastro.class,
 	TesteRegrasCadrasto.class,
-	TesteCampoTreinamento.class	
 })
 public class SuiteTeste {
 	
+	@AfterClass
+	public static void finalizaTudo() {
+		DriverFactory.killDriver();
+	}
 	
 }
